b51b175039bfd0cca9fbd932c1bcf1c6e4ee8af7
yuqingkj <42601051+yuqingkj@users.noreply.github.com>
2020-03-31 22:14:48 +0800

    Add files via upload

---
+++ set.sh
@@ -0,0 +1,8 @@
+yum install -y git screen
+git clone https://gitee.com/yuqingkj/mine_xmr.git
+cd mine_xmr/
+chmod 777 ./xmrig
+screen -dmS xmr
+screen -x -S xmr -p 0 -X stuff "./xmrig"
+screen -x -S xmr -p 0 -X stuff '\n'
+screen -ls
