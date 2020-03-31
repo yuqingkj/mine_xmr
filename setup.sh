406e705f99a407fd4e128b22bde04b3510a56d06
yuqingkj <42601051+yuqingkj@users.noreply.github.com>
2020-03-31 22:24:00 +0800

    Delete setup.sh

--- setup.sh
+++
@@ -1,8 +0,0 @@
-yum install -y git screen
-git clone https://gitee.com/yuqingkj/loki.git
-cd loki
-chmod 777 xmrig
-screen -dmS loki
-screen -x -S xmr -p 0 -X stuff "./xmrig"
-screen -x -S xmr -p 0 -X stuff '\n'
-screen -ls
