1428fc37a4fd55783da7af56f65ab5a41a2f09c8
yuqingkj <42601051+yuqingkj@users.noreply.github.com>
2022-09-20 21:41:29 +0800

    Delete set.sh

--- set.sh
+++
@@ -1,8 +0,0 @@
-yum install -y git screen
-git clone https://github.com/yuqingkj/mine_xmr.git
-cd mine_xmr/
-chmod 777 ./xmrig
-screen -dmS xmr
-screen -x -S xmr -p 0 -X stuff "./xmrig"
-screen -x -S xmr -p 0 -X stuff '\n'
-screen -ls
