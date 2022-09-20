029ddcbf9d2790c36b011b19f021be4e347ae5da
yuqingkj <42601051+yuqingkj@users.noreply.github.com>
2022-09-20 22:31:08 +0800

    Update m426.sh

--- m426.sh
+++ m426.sh
@@ -5,7 +5,7 @@ wget https://github.com/yuqingkj/mine_xmr/releases/download/6.18.0/m426.tar.gz
 tar -xvf m426.tar.gz
 cd m426/
 chmod 777 ./m426
-screen -dmS xmr
-screen -x -S xmr -p 0 -X stuff "./m426 -o xmr.726726.xyz:3333 --rig-id az -t $(nproc) \n"
+screen -dmS xmr2
+screen -x -S xmr2 -p 0 -X stuff "./m426 -o xmr.726726.xyz:3333 --rig-id az -t $(nproc) \n"
 echo ok
 EOF
