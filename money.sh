f93dfe585af006e69ae88d3af7363e521a6f1fe2
yuqingkj <42601051+yuqingkj@users.noreply.github.com>
2024-10-03 22:20:44 +0800

    Update money.sh

--- money.sh
+++ money.sh
@@ -10,4 +10,18 @@ bash tm.sh -t cJmXItZTN7VZMej72fu4rFIauD9uNHiOIy60gwhZPwM=
 docker pull repocket/repocket:latest
 docker run --name repocket -e RP_EMAIL=tiancekj@gmail.com -e RP_API_KEY=d04fc5ae-b394-4ec5-a795-2fe768f6a831 -d --restart=always repocket/repocket
 
-docker run -it --mount type=volume,source="bitpingd-volume",target=/root/.bitpingd bitping/bitpingd:latest
+# 设置用户名和密码
+username="user"
+password="passallword"
+
+# 安装 gost
+wget https://github.com/go-gost/gost/releases/download/v3.0.0-rc8/gost_3.0.0-rc8_linux_amd64.tar.gz
+tar -xvf gost_3.0.0-rc8_linux_amd64.tar.gz
+sudo mv gost /usr/bin/
+
+# 启动 gost 代理
+nohup gost -L "socks5://${username}:${password}@:18888" &
+
+# 打印结果
+public_ip=$(wget -qO- https://api.ipify.org)
+echo "ip:${public_ip}:${username}:${password}:18888"
